aff955d8f0deef595f61340864eeada2fa3982d8
Salman <salkhan23@gmail.com>
2019-07-31 19:41:14 -0400

    Add a script to generate the dataset

--- generate_data_set.py
+++ generate_data_set.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from datetime import datetime
+import sys
+import shutil
 
 import gabor_fits
 import generate_stimuli
@@ -11,20 +15,15 @@ if __name__ == "__main__":
     # -----------------------------------------------------------------------------------
     random_seed = 10
 
-    contour_len = 7
-    beta_rotation = 0
-    alpha_rotation = 0
-
     fragment_size = np.array([11, 11])
     full_tile_size = np.array([18, 18])
-
     image_size = np.array([227, 227, 3])
 
     # Gabor Fragment
     gabor_parameters = [{
         'x0': 0,
         'y0': 0,
-        'theta_deg': 60,
+        'theta_deg': 90,
         'amp': 1,
         'sigma': 2.0,
         'lambda1': 6,
@@ -34,81 +33,112 @@ if __name__ == "__main__":
 
     fragment = gabor_fits.get_gabor_fragment(gabor_parameters, fragment_size)
 
+    base_dir = './data/curved_contours/train'
+    n_images_per_set = 100
+
+    cont_len_arr = [3, 5, 7, 9]
+    beta_rot_arr = [0, 15, 30]
+    alpha_rot_arr = [0]
+
+    # ---------------------------------------------
     # Immutable
     plt.ion()
     np.random.seed(random_seed)
 
     # -----------------------------------------------------------------------------------
-    #  Stimuli Generation
+    # Generate multiple images and store data and labels in directory
     # -----------------------------------------------------------------------------------
-    bg_value = generate_stimuli.get_mean_pixel_value_at_boundary(fragment)
-    test_image = np.ones(image_size, dtype=np.uint8) * bg_value
+    if os.path.exists(base_dir):
+        ans = input("Data already exists in {}.\nEnter y to overwrite".format(base_dir))
+        if 'y' in ans.lower():
+            shutil.rmtree(base_dir)
+        else:
+            sys.exit()
 
-    # Get background tile locations
-    center_full_tile_start = (image_size[:2] // 2) - (full_tile_size // 2)
-    bg_frag_starts = generate_stimuli.get_background_tiles_locations(
-        frag_len=full_tile_size[0],
-        img_len=image_size[0],
-        row_offset=0,
-        space_bw_tiles=0,
-        tgt_n_visual_rf_start=center_full_tile_start[0]
-    )
+    data_store_dir = os.path.join(base_dir, 'images')
+    labels_store_dir = os.path.join(base_dir, 'labels')
 
-    # Get the min, max starting offsets so that the defined contours stays within the image
-    x_start_range, y_start_range = generate_stimuli.get_contour_start_ranges(
-        c_len=contour_len,
-        frag_orient=gabor_parameters[0]['theta_deg'],  # Todo handle the case when there are three orientations
-        f_tile_size=full_tile_size,
-        img_size=image_size
-    )
+    start_time = datetime.now()
+    n_total_images = 0
+
+    if not os.path.exists(data_store_dir):
+        os.makedirs(data_store_dir)
+    if not os.path.exists(labels_store_dir):
+        os.makedirs(labels_store_dir)
+
+    for c_len in cont_len_arr:
+        c_len_name = 'clen_{}'.format(c_len)
+
+        x_start_range, y_start_range = generate_stimuli.get_contour_start_ranges(
+            c_len=c_len,
+            frag_orient=gabor_parameters[0]['theta_deg'],  # Todo handle the case when there are three orientations
+            f_tile_size=full_tile_size,
+            img_size=image_size
+        )
+
+        for beta in beta_rot_arr:
+            c_len_beta_rot_dir = os.path.join(c_len_name, 'beta_{}'.format(beta))
+
+            for alpha in alpha_rot_arr:
+                c_len_beta_rot_alpha_rot_dir = os.path.join(c_len_beta_rot_dir, 'alpha_{}'.format(alpha))
+
+                store_data_dir_full = os.path.join(data_store_dir, c_len_beta_rot_alpha_rot_dir)
+                store_label_dir_full = os.path.join(labels_store_dir, c_len_beta_rot_alpha_rot_dir)
+
+                if not os.path.exists(store_data_dir_full):
+                    os.makedirs(store_data_dir_full)
+                if not os.path.exists(store_label_dir_full):
+                    os.makedirs(store_label_dir_full)
+
+                print("Generating {} images with c_len = {}, beta = {}, alpha = {}".format(
+                    n_images_per_set, c_len, beta, alpha))
+
+                for i_idx in range(n_images_per_set):
+
+                    center_frag_start = np.array([
+                        np.random.randint(x_start_range[0], x_start_range[1]),
+                        np.random.randint(y_start_range[0], y_start_range[1]),
+                    ])
+
+                    img, img_label = generate_stimuli.generate_contour_image(
+                        frag=fragment,
+                        frag_params=gabor_parameters,
+                        c_len=c_len,
+                        beta=beta,
+                        alpha=alpha,
+                        f_tile_size=full_tile_size,
+                        img_size=image_size,
+                        random_alpha_rot=False,
+                        center_frag_start=center_frag_start
+                    )
+
+                    # Save
+                    file_name = 'clen_{}_beta_{}_alpha_{}_{}'.format(c_len, beta, alpha, i_idx)
+                    plt.imsave(fname=os.path.join(store_data_dir_full, file_name + '.png'), arr=img, format='PNG')
+                    np.save(file=os.path.join(store_label_dir_full, file_name + '.npy'), arr=img_label)
+
+                    n_total_images += 1
 
     # -------------------------------------------------------------------------------------
-    image, image_label = generate_stimuli.generate_contour_image(
-        frag=fragment,
-        frag_params=gabor_parameters,
-        c_len=contour_len,
-        beta=beta_rotation,
-        alpha=alpha_rotation,
-        f_tile_size=full_tile_size,
-        img_size=image_size,
-        random_alpha_rot=False,
-        center_frag_start=np.array([x_start_range[0], y_start_range[0]])
-    )
-
-    plt.figure()
-    plt.imshow(image)
-    plt.title("Input Image")
-
-    # Highlight the label
-    center_full_tile_start = image_size[:2] // 2 - (full_tile_size[0:2] // 2)
-    full_tile_starts = generate_stimuli.get_background_tiles_locations(
-        frag_len=full_tile_size[0],
-        img_len=image_size[0],
-        row_offset=0,
-        space_bw_tiles=0,
-        tgt_n_visual_rf_start=center_full_tile_start[0]
-    )
-
-    contour_containing_tiles = full_tile_starts[image_label.flatten().nonzero()]
-    labeled_image = generate_stimuli.highlight_tiles(image, full_tile_size, contour_containing_tiles)
-
-    plt.figure()
-    plt.imshow(labeled_image)
-    plt.title("Labeled image")
-
-    input("press any key to exit")
-
-
-
-
-
-
-
-
-    # # -----------------------------------------------------------------------------------
-    # center_full_tile_start = (image_size[:2] // 2) - (full_tile_size // 2)
+    # image, image_label = generate_stimuli.generate_contour_image(
+    #     frag=fragment,
+    #     frag_params=gabor_parameters,
+    #     c_len=contour_len,
+    #     beta=beta_rotation,
+    #     alpha=alpha_rotation,
+    #     f_tile_size=full_tile_size,
+    #     img_size=image_size,
+    #     random_alpha_rot=False,
+    #     center_frag_start=np.array([x_start_range[0], y_start_range[0]])
+    # )
     #
-    # bg_frag_starts = generate_stimuli.get_background_tiles_locations(
+    # plt.figure()
+    # plt.imshow(image)
+    # plt.title("Input Image")
+    #
+    # # Highlight the label
+    # center_full_tile_start = image_size[:2] // 2 - (full_tile_size[0:2] // 2)
+    # full_tile_starts = generate_stimuli.get_background_tiles_locations(
     #     frag_len=full_tile_size[0],
     #     img_len=image_size[0],
     #     row_offset=0,
@@ -116,69 +146,13 @@ if __name__ == "__main__":
     #     tgt_n_visual_rf_start=center_full_tile_start[0]
     # )
     #
-
-    # #  Add the Contour Path
-    # test_image, path_fragment_starts = generate_stimuli.add_contour_path_constant_separation(
-    #     img=test_image,
-    #     frag=fragment,
-    #     frag_params=gabor_parameters,
-    #     c_len=contour_len,
-    #     beta=beta_rotation,
-    #     alpha=alpha_rotation,
-    #     d=full_tile_size[0],
-    #     rand_inter_frag_direction_change=True,
-    #     base_contour='sigmoid',
-    #     center_frag_start=np.array([180,150])
-    # )
-    #
-    # # Generate the label
-    # labels = np.zeros(len(bg_frag_starts))
-    # bg_frag_centers = bg_frag_starts + full_tile_size // 2
-    #
-    # # Find the full tile in which contour fragments lie (mostly)
-    # for path_frag_start in path_fragment_starts:
-    #
-    #     path_frag_center = path_frag_start + fragment_size // 2
-    #
-    #     dist_to_c_frag = np.linalg.norm(bg_frag_centers - path_frag_center, axis=1)
-    #     closest_full_tile_idx = np.argmin(dist_to_c_frag)
-    #
-    #     print(closest_full_tile_idx, dist_to_c_frag[closest_full_tile_idx])
-    #
-    #     labels[closest_full_tile_idx] = 1
-    #
-    # z = np.reshape(labels, (13, 13))
-    # print(z)
-    #
-    #
-    # img = generate_stimuli.highlight_tiles(test_image, full_tile_size, bg_frag_starts, edge_color=(0, 255, 0))
+    # contour_containing_tiles = full_tile_starts[image_label.flatten().nonzero()]
+    # labeled_image = generate_stimuli.highlight_tiles(image, full_tile_size, contour_containing_tiles)
     #
     # plt.figure()
-    # plt.imshow(img)
-    #
-    # import pdb
-    # pdb.set_trace()
-    #
-    # # # -----------------------------------------------------------------------------------
-    # # #  Stimulus - Single function
-    # # # -----------------------------------------------------------------------------------
-    # # contour_len = 9
-    # # beta_rotation = 15
-    # # alpha_rotation = 0
-    # #
-    # # img_arr = generate_stimuli.generate_contour_images(
-    # #     n_images=1,
-    # #     frag=fragment,
-    # #     frag_params=gabor_parameters,
-    # #     c_len=contour_len,
-    # #     beta=beta_rotation,
-    # #     alpha=alpha_rotation,
-    # #     f_tile_size=full_tile_size,
-    # #     img_size=np.array((227, 227, 3)),
-    # #     random_alpha_rot=True
-    # # )
-    # #
-    # # plt.figure()
-    # # image_idx = 0
-    # # plt.imshow(img_arr[image_idx, :])
-    # # plt.title("Image @ index {}".format(image_idx))
+    # plt.imshow(labeled_image)
+    # plt.title("Labeled image")
+
+    # -----------------------------------------------------------------------------------
+    print("{} Images created. Data Generation took {}".format(n_total_images, datetime.now() - start_time))
+    input("press any key to exit")
