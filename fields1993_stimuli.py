a89344d2a27e26f8ee5520d82d30ed1580eb219a
Salman <salkhan23@gmail.com>
2019-08-01 12:56:34 -0400

    Move dataset generation into a function and move to fields1993 stimuli file. 2. the data generation script now generates train and test data sets

--- fields1993_stimuli.py
+++ fields1993_stimuli.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import gabor_fits
 import copy
 from skimage.transform import rotate
+import os
+from datetime import datetime
+import sys
+import shutil
 
 
 def get_2d_gaussian_kernel(shape, sigma=1.0):
@@ -809,6 +813,108 @@ def get_contour_start_ranges(c_len, frag_orient, f_tile_size, img_size):
     #     min_start_x, max_start_x, min_start_y, max_start_y))
 
     return (min_start_x, max_start_x), (min_start_y, max_start_y)
+
+
+def generate_data_set(
+        n_imgs_per_set, base_dir, frag, frag_params, c_len_arr, beta_rot_arr, alpha_rot_arr, f_tile_size,
+        img_size=None):
+    """
+     Generate Data Set
+     TODO: handle the case when multiple gabor fragments / tile sizes are defined.
+     TODO: handle the getting orientation for a Gabor with Three channels.
+
+    :param n_imgs_per_set:
+    :param base_dir:
+    :param frag:
+    :param frag_params:
+    :param c_len_arr:
+    :param beta_rot_arr:
+    :param alpha_rot_arr:
+    :param f_tile_size:
+    :param img_size:
+    :return:
+    """
+
+    if os.path.exists(base_dir):
+        ans = input("Data already exists in {}.\nEnter y to overwrite".format(base_dir))
+        if 'y' in ans.lower():
+            shutil.rmtree(base_dir)
+        else:
+            sys.exit()
+
+    # Stores the path to all files in the image_set common to images and labels
+    data_key = 'data_key.txt'
+
+    data_store_dir = os.path.join(base_dir, 'images')
+    labels_store_dir = os.path.join(base_dir, 'labels')
+    data_key_file = os.path.join(base_dir, data_key)
+
+    start_time = datetime.now()
+    n_total_imgs = 0
+
+    if not os.path.exists(data_store_dir):
+        os.makedirs(data_store_dir)
+    if not os.path.exists(labels_store_dir):
+        os.makedirs(labels_store_dir)
+
+    f = open(data_key_file, 'w+')
+
+    for c_len in c_len_arr:
+        c_len_name = 'clen_{}'.format(c_len)
+
+        x_start_range, y_start_range = get_contour_start_ranges(
+            c_len=c_len,
+            frag_orient=frag_params[0]['theta_deg'],  # Todo handle the case when there are three orientations
+            f_tile_size=f_tile_size,
+            img_size=img_size
+        )
+
+        for beta in beta_rot_arr:
+            c_len_beta_rot_dir = os.path.join(c_len_name, 'beta_{}'.format(beta))
+
+            for alpha in alpha_rot_arr:
+                c_len_beta_rot_alpha_rot_dir = os.path.join(c_len_beta_rot_dir, 'alpha_{}'.format(alpha))
+
+                store_data_dir_full = os.path.join(data_store_dir, c_len_beta_rot_alpha_rot_dir)
+                store_label_dir_full = os.path.join(labels_store_dir, c_len_beta_rot_alpha_rot_dir)
+
+                if not os.path.exists(store_data_dir_full):
+                    os.makedirs(store_data_dir_full)
+                if not os.path.exists(store_label_dir_full):
+                    os.makedirs(store_label_dir_full)
+
+                print("Generating {} images with c_len = {}, beta = {}, alpha = {}".format(
+                    n_imgs_per_set, c_len, beta, alpha))
+
+                for i_idx in range(n_imgs_per_set):
+                    center_frag_start = np.array([
+                        np.random.randint(x_start_range[0], x_start_range[1]),
+                        np.random.randint(y_start_range[0], y_start_range[1]),
+                    ])
+
+                    img, img_label = generate_contour_image(
+                        frag=frag,
+                        frag_params=frag_params,
+                        c_len=c_len,
+                        beta=beta,
+                        alpha=alpha,
+                        f_tile_size=f_tile_size,
+                        img_size=img_size,
+                        random_alpha_rot=False,
+                        center_frag_start=center_frag_start,
+                        base_contour='random'
+                    )
+
+                    # Save
+                    file_name = 'clen_{}_beta_{}_alpha_{}_{}'.format(c_len, beta, alpha, i_idx)
+                    plt.imsave(fname=os.path.join(store_data_dir_full, file_name + '.png'), arr=img, format='PNG')
+                    np.save(file=os.path.join(store_label_dir_full, file_name + '.npy'), arr=img_label)
+                    f.write(os.path.join(c_len_beta_rot_alpha_rot_dir, file_name) + '\n')
+
+                    n_total_imgs += 1
+    f.close()
+    print(" Data set created @ {}. Contains {} Images. Time Taken {}".format(
+          base_dir, n_total_imgs, datetime.now() - start_time))
 
 
 if __name__ == "__main__":
--- generate_data_set.py
+++ generate_data_set.py
@@ -1,9 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-from datetime import datetime
-import sys
-import shutil
 
 import gabor_fits
 import fields1993_stimuli
@@ -14,8 +10,10 @@ if __name__ == "__main__":
     # Initialization
     # -----------------------------------------------------------------------------------
     random_seed = 10
+    plt.ion()
+    np.random.seed(random_seed)
 
-    fragment_size = np.array([11, 11])
+    frag_size = np.array([11, 11])
     full_tile_size = np.array([18, 18])
     image_size = np.array([227, 227, 3])
 
@@ -31,101 +29,130 @@ if __name__ == "__main__":
         'gamma': 1
     }]
 
-    fragment = gabor_fits.get_gabor_fragment(gabor_parameters, fragment_size)
+    fragment = gabor_fits.get_gabor_fragment(gabor_parameters, frag_size)
 
-    base_dir = './data/curved_contours/train'
-    n_images_per_set = 100
-
-    cont_len_arr = [3, 5, 7, 9]
-    beta_rot_arr = [0, 15, 30]
-    alpha_rot_arr = [0]
+    contour_len_arr = [3, 5, 7, 9]
+    beta_rotation_arr = [0, 15, 30]
+    alpha_rotation_arr = [0]
 
     # ---------------------------------------------
-    # Immutable
-    plt.ion()
-    np.random.seed(random_seed)
+    # Generate the training Set
+    data_dir = './data/curved_contours/train'
+    num_images_per_set = 300
+
+    fields1993_stimuli.generate_data_set(
+        n_imgs_per_set=num_images_per_set,
+        base_dir=data_dir,
+        frag=fragment,
+        frag_params=gabor_parameters,
+        c_len_arr=contour_len_arr,
+        beta_rot_arr=beta_rotation_arr,
+        alpha_rot_arr=alpha_rotation_arr,
+        f_tile_size=full_tile_size,
+        img_size=image_size,
+    )
+
+    # Generate the Test Set
+    data_dir = './data/curved_contours/test'
+    num_images_per_set = 50
+
+    fields1993_stimuli.generate_data_set(
+        n_imgs_per_set=num_images_per_set,
+        base_dir=data_dir,
+        frag=fragment,
+        frag_params=gabor_parameters,
+        c_len_arr=contour_len_arr,
+        beta_rot_arr=beta_rotation_arr,
+        alpha_rot_arr=alpha_rotation_arr,
+        f_tile_size=full_tile_size,
+        img_size=image_size,
+    )
+
+
+
+
+    #
+    # # -----------------------------------------------------------------------------------
+    # # Generate multiple images and store data and labels in directory
+    # # -----------------------------------------------------------------------------------
+    # if os.path.exists(base_dir):
+    #     ans = input("Data already exists in {}.\nEnter y to overwrite".format(base_dir))
+    #     if 'y' in ans.lower():
+    #         shutil.rmtree(base_dir)
+    #     else:
+    #         sys.exit()
+    #
+    # data_key = 'data_key.txt'  # Stores the path to all files in the image_set
+    #
+    # data_store_dir = os.path.join(base_dir, 'images')
+    # labels_store_dir = os.path.join(base_dir, 'labels')
+    # data_key_file = os.path.join(base_dir, data_key)
+    #
+    # start_time = datetime.now()
+    # n_total_images = 0
+    #
+    # if not os.path.exists(data_store_dir):
+    #     os.makedirs(data_store_dir)
+    # if not os.path.exists(labels_store_dir):
+    #     os.makedirs(labels_store_dir)
+    #
+    # f = open(data_key_file, 'w+')
+    #
+    # for c_len in c_len_arr:
+    #     c_len_name = 'clen_{}'.format(c_len)
+    #
+    #     x_start_range, y_start_range = fields1993_stimuli.get_contour_start_ranges(
+    #         c_len=c_len,
+    #         frag_orient=gabor_params[0]['theta_deg'],  # Todo handle the case when there are three orientations
+    #         f_tile_size=f_tile_size,
+    #         img_size=image_size
+    #     )
+    #
+    #     for beta in beta_rot_arr:
+    #         c_len_beta_rot_dir = os.path.join(c_len_name, 'beta_{}'.format(beta))
+    #
+    #         for alpha in alpha_rot_arr:
+    #             c_len_beta_rot_alpha_rot_dir = os.path.join(c_len_beta_rot_dir, 'alpha_{}'.format(alpha))
+    #
+    #             store_data_dir_full = os.path.join(data_store_dir, c_len_beta_rot_alpha_rot_dir)
+    #             store_label_dir_full = os.path.join(labels_store_dir, c_len_beta_rot_alpha_rot_dir)
+    #
+    #             if not os.path.exists(store_data_dir_full):
+    #                 os.makedirs(store_data_dir_full)
+    #             if not os.path.exists(store_label_dir_full):
+    #                 os.makedirs(store_label_dir_full)
+    #
+    #             print("Generating {} images with c_len = {}, beta = {}, alpha = {}".format(
+    #                 n_imgs_per_set, c_len, beta, alpha))
+    #
+    #             for i_idx in range(n_imgs_per_set):
+    #
+    #                 center_frag_start = np.array([
+    #                     np.random.randint(x_start_range[0], x_start_range[1]),
+    #                     np.random.randint(y_start_range[0], y_start_range[1]),
+    #                 ])
+    #
+    #                 img, img_label = fields1993_stimuli.generate_contour_image(
+    #                     frag=frag,
+    #                     frag_params=gabor_params,
+    #                     c_len=c_len,
+    #                     beta=beta,
+    #                     alpha=alpha,
+    #                     f_tile_size=f_tile_size,
+    #                     img_size=image_size,
+    #                     random_alpha_rot=False,
+    #                     center_frag_start=center_frag_start
+    #                 )
+    #
+    #                 # Save
+    #                 file_name = 'clen_{}_beta_{}_alpha_{}_{}'.format(c_len, beta, alpha, i_idx)
+    #                 plt.imsave(fname=os.path.join(store_data_dir_full, file_name + '.png'), arr=img, format='PNG')
+    #                 np.save(file=os.path.join(store_label_dir_full, file_name + '.npy'), arr=img_label)
+    #                 f.write(os.path.join(c_len_beta_rot_alpha_rot_dir, file_name) + '\n')
+    #
+    #                 n_total_images += 1
+    # f.close()
 
     # -----------------------------------------------------------------------------------
-    # Generate multiple images and store data and labels in directory
-    # -----------------------------------------------------------------------------------
-    if os.path.exists(base_dir):
-        ans = input("Data already exists in {}.\nEnter y to overwrite".format(base_dir))
-        if 'y' in ans.lower():
-            shutil.rmtree(base_dir)
-        else:
-            sys.exit()
 
-    data_key = 'data_key.txt'  # Stores the path to all files in the image_set
-
-    data_store_dir = os.path.join(base_dir, 'images')
-    labels_store_dir = os.path.join(base_dir, 'labels')
-    data_key_file = os.path.join(base_dir, data_key)
-
-    start_time = datetime.now()
-    n_total_images = 0
-
-    if not os.path.exists(data_store_dir):
-        os.makedirs(data_store_dir)
-    if not os.path.exists(labels_store_dir):
-        os.makedirs(labels_store_dir)
-
-    f = open(data_key_file, 'w+')
-
-    for c_len in cont_len_arr:
-        c_len_name = 'clen_{}'.format(c_len)
-
-        x_start_range, y_start_range = fields1993_stimuli.get_contour_start_ranges(
-            c_len=c_len,
-            frag_orient=gabor_parameters[0]['theta_deg'],  # Todo handle the case when there are three orientations
-            f_tile_size=full_tile_size,
-            img_size=image_size
-        )
-
-        for beta in beta_rot_arr:
-            c_len_beta_rot_dir = os.path.join(c_len_name, 'beta_{}'.format(beta))
-
-            for alpha in alpha_rot_arr:
-                c_len_beta_rot_alpha_rot_dir = os.path.join(c_len_beta_rot_dir, 'alpha_{}'.format(alpha))
-
-                store_data_dir_full = os.path.join(data_store_dir, c_len_beta_rot_alpha_rot_dir)
-                store_label_dir_full = os.path.join(labels_store_dir, c_len_beta_rot_alpha_rot_dir)
-
-                if not os.path.exists(store_data_dir_full):
-                    os.makedirs(store_data_dir_full)
-                if not os.path.exists(store_label_dir_full):
-                    os.makedirs(store_label_dir_full)
-
-                print("Generating {} images with c_len = {}, beta = {}, alpha = {}".format(
-                    n_images_per_set, c_len, beta, alpha))
-
-                for i_idx in range(n_images_per_set):
-
-                    center_frag_start = np.array([
-                        np.random.randint(x_start_range[0], x_start_range[1]),
-                        np.random.randint(y_start_range[0], y_start_range[1]),
-                    ])
-
-                    img, img_label = fields1993_stimuli.generate_contour_image(
-                        frag=fragment,
-                        frag_params=gabor_parameters,
-                        c_len=c_len,
-                        beta=beta,
-                        alpha=alpha,
-                        f_tile_size=full_tile_size,
-                        img_size=image_size,
-                        random_alpha_rot=False,
-                        center_frag_start=center_frag_start
-                    )
-
-                    # Save
-                    file_name = 'clen_{}_beta_{}_alpha_{}_{}'.format(c_len, beta, alpha, i_idx)
-                    plt.imsave(fname=os.path.join(store_data_dir_full, file_name + '.png'), arr=img, format='PNG')
-                    np.save(file=os.path.join(store_label_dir_full, file_name + '.npy'), arr=img_label)
-                    f.write(os.path.join(c_len_beta_rot_alpha_rot_dir, file_name) + '\n')
-
-                    n_total_images += 1
-    f.close()
-
-    # -----------------------------------------------------------------------------------
-    print("{} Images created. Data Generation took {}".format(n_total_images, datetime.now() - start_time))
     input("press any key to exit")
