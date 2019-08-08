327138e99f411fe6d5adacd5698b315d3a9a40ab
Salman <salkhan23@gmail.com>
2019-08-08 15:55:23 -0400

    Add a new type of control model that matches the number of iterations

--- models/control_model.py
+++ models/control_model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as nn_functional
 import torchvision
@@ -6,10 +5,11 @@ import torchvision
 from .cont_int_model import ClassifierHead
 
 
-class ControlModel(nn.Module):
+class CmMatchParameters(nn.Module):
     def __init__(self, edge_out_ch=64, ):
+        """ Match the number of parameters """
 
-        super(ControlModel, self).__init__()
+        super(CmMatchParameters, self).__init__()
 
         # Parameters
         self.edge_out_ch = edge_out_ch
@@ -51,9 +51,7 @@ class ControlModel(nn.Module):
         ff = self.bn1(ff)
         ff = nn.functional.relu(ff)
 
-        # -------------------------------------------------------------------------------
-        # Control Layer
-        # -------------------------------------------------------------------------------
+        # Control - Contour Integration equivalent
         out_layer_arr = []
 
         ff = self.control_conv1(ff)
@@ -67,6 +65,65 @@ class ControlModel(nn.Module):
         ff = nn.functional.relu(ff)
         out_layer_arr.append(self.post(ff))
         ff = self.control_dp2(ff)
+
+        # Post processing
+        out = self.post(ff)
+
+        return out, out_layer_arr
+
+
+class CmMatchIterations(nn.Module):
+    def __init__(self, edge_out_ch=64, n_iters=5):
+        """ Match the number of parameters """
+
+        super(CmMatchIterations, self).__init__()
+
+        # Parameters
+        self.edge_out_ch = edge_out_ch
+        self.n_iters = n_iters
+
+        # Layers
+        # ------
+        # First layer is AlexNet Edge Extraction (with out bias)
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=edge_out_ch,
+            kernel_size=11,
+            stride=4,
+            padding=2,
+            bias=False
+        )
+        # use pre-trained Alexnet weights
+        alexnet_edge_detect_kernels = torchvision.models.alexnet(pretrained=True).features[0]
+        self.conv1.weight.data.copy_(alexnet_edge_detect_kernels.weight.data)
+
+        # Additional Batch normalization Layer
+        self.bn1 = nn.BatchNorm2d(num_features=64)
+
+        self.control_conv1 = nn.Conv2d(
+            in_channels=edge_out_ch, out_channels=edge_out_ch, kernel_size=7, stride=1, padding=3, bias=False)
+        self.control_bn1 = nn.BatchNorm2d(num_features=edge_out_ch)
+        self.control_dp1 = nn.Dropout(p=0.3)
+
+        self.post = ClassifierHead(edge_out_ch)
+
+    def forward(self, in_img):
+
+        # Edge Extraction
+        ff = self.conv1(in_img)
+        ff = self.bn1(ff)
+        ff = nn.functional.relu(ff)
+
+        # Control - Contour Integration equivalent
+        out_layer_arr = []
+
+        for i in range(self.n_iters):
+            ff = self.control_conv1(ff)
+            ff = self.control_bn1(ff)
+            ff = nn.functional.relu(ff)
+            out_layer_arr.append(self.post(ff))
+
+            ff = self.control_dp1(ff)
 
         # Post processing
         out = self.post(ff)
