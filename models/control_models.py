42585dc76f4fef5ecf3acf1df655c5a4e84d79b1
Salman <salkhan23@gmail.com>
2019-08-16 18:16:31 -0400

    Add a new control model that uses only the control head. Helps validate that the classification head is not good enough to do the task on its own

--- models/control_models.py
+++ models/control_models.py
@@ -129,3 +129,50 @@ class CmMatchIterations(nn.Module):
         out = self.post(ff)
 
         return out, out_layer_arr
+
+
+class CmClassificationHeadOnly(nn.Module):
+    def __init__(self, edge_out_ch=64, ):
+        """ Simplest of control models, lateral layer is completely removed.
+            THe purpose of this model is to ensure the classification head is not good enough to do
+            the task on its own.
+        """
+
+        super(CmClassificationHeadOnly, self).__init__()
+
+        # Parameters
+        self.edge_out_ch = edge_out_ch
+
+        # Layers
+        # ------
+        # First layer is AlexNet Edge Extraction (with out bias)
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=edge_out_ch,
+            kernel_size=11,
+            stride=4,
+            padding=2,
+            bias=False
+        )
+        # use pre-trained Alexnet weights
+        alexnet_edge_detect_kernels = torchvision.models.alexnet(pretrained=True).features[0]
+        self.conv1.weight.data.copy_(alexnet_edge_detect_kernels.weight.data)
+
+        # Additional Batch normalization Layer
+        self.bn1 = nn.BatchNorm2d(num_features=64)
+        self.post = ClassifierHead(edge_out_ch)
+
+    def forward(self, in_img):
+
+        # Edge Extraction
+        ff = self.conv1(in_img)
+        ff = self.bn1(ff)
+        ff = nn.functional.relu(ff)
+
+        # Control - Contour Integration equivalent
+        out_layer_arr = []
+
+        # Post processing
+        out = self.post(ff)
+
+        return out, out_layer_arr
