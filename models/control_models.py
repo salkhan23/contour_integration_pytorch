30c39263aa2a8e45a50a8cd88bc47e9d1dc4ed2d
Salman <salkhan23@gmail.com>
2019-08-25 12:59:38 -0400

    Try a different, iterative decoding model. One with match number of paramters and conv layers. The only thing missing is the local circuit

--- models/control_models.py
+++ models/control_models.py
@@ -74,7 +74,13 @@ class CmMatchParameters(nn.Module):
 
 class CmMatchIterations(nn.Module):
     def __init__(self, edge_out_ch=64, n_iters=5):
-        """ Match the number of parameters """
+        """
+        A Re-current model with matching number of recurrent iterations and parameters
+        Parameters are matched by  using a two convolutional layers whose activities are subtracted
+        before the batch normalization layer
+
+        Role: Investigate the impact of the local circuit (which is not present in this model)
+        """
 
         super(CmMatchIterations, self).__init__()
 
@@ -93,6 +99,7 @@ class CmMatchIterations(nn.Module):
             padding=2,
             bias=False
         )
+
         # use pre-trained Alexnet weights
         alexnet_edge_detect_kernels = torchvision.models.alexnet(pretrained=True).features[0]
         self.conv1.weight.data.copy_(alexnet_edge_detect_kernels.weight.data)
@@ -102,6 +109,9 @@ class CmMatchIterations(nn.Module):
 
         self.control_conv1 = nn.Conv2d(
             in_channels=edge_out_ch, out_channels=edge_out_ch, kernel_size=7, stride=1, padding=3, bias=False)
+        self.control_conv2 = nn.Conv2d(
+            in_channels=edge_out_ch, out_channels=edge_out_ch, kernel_size=7, stride=1, padding=3, bias=False)
+
         self.control_bn1 = nn.BatchNorm2d(num_features=edge_out_ch)
         self.control_dp1 = nn.Dropout(p=0.3)
 
@@ -118,8 +128,10 @@ class CmMatchIterations(nn.Module):
         out_layer_arr = []
 
         for i in range(self.n_iters):
-            ff = self.control_conv1(ff)
-            ff = self.control_bn1(ff)
+            ff_branch_a = self.control_conv1(ff)
+            ff_branch_b = self.control_conv2(ff)
+
+            ff = self.control_bn1(ff_branch_a - ff_branch_b)
             ff = nn.functional.relu(ff)
             out_layer_arr.append(self.post(ff))
 
