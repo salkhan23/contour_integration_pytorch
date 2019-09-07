ec98eb6ddc596060f9a8ea656f402205f0df582c
Salman <salkhan23@gmail.com>
2019-09-07 14:27:46 -0400

    Save the .tar file at the correct location

--- train_imagenet.py
+++ train_imagenet.py
@@ -401,7 +401,7 @@ def validate(val_loader, model, criterion, args):
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+        shutil.copyfile(filename,  os.path.join(os.path.dirname(filename), 'model_best.pth.tar'))
 
 
 class AverageMeter(object):
