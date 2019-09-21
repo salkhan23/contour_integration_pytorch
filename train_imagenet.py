4aa95f9176eff5618b0bed1e929524805f892067
Salman <salkhan23@gmail.com>
2019-09-21 10:48:31 -0400

    enable training of resnet + cont int model

--- train_imagenet.py
+++ train_imagenet.py
@@ -582,18 +582,18 @@ if __name__ == '__main__':
     # -----------------------------------------------------------------------------------
     print(">>> Building the model {}".format('.' * 80))
 
-    # # Contour Integration model
-    # import models.piech_models
-    # cont_int_model = models.piech_models.CurrentSubtractiveInhibition(use_class_head=False)
+    # Contour Integration model
+    import models.piech_models
+    cont_int_model = models.piech_models.CurrentSubtractiveInhibition(use_class_head=False)
     #
     # # # Control Model
     # # import models.control_models
     # # cont_int_model = models.control_models.CmMatchParameters(use_class_head=False)
     #
-    # # net = embed_resnet50(cont_int_model)
+    net = embed_resnet50(cont_int_model)
     # net = embed_alexnet(cont_int_model, pretrained=False)
 
-    net = torchvision_models.resnet50(pretrained=True)
+    # net = torchvision_models.resnet50(pretrained=True)
 
     print(">>> Starting main script {}".format('.'*80))
     main(net)
