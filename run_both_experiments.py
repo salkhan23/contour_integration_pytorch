a1171f6494743e27729c5e29c687c3362e90242d
Salman <salkhan23@gmail.com>
2020-03-04 20:37:29 -0500

    Minor: Tidy up script. Label the different types of models. 2. Reenable the fragment spacing experiment and add the default fragment size in the fragment_size_list

--- run_both_experiments.py
+++ run_both_experiments.py
@@ -22,35 +22,49 @@ if __name__ == "__main__":
     # Loading Saved Model
     print("===> Loading Model ...")
 
-    # net.load_state_dict(torch.load(saved_model))
-    # # Model trained with 5 iterations
+    # # Contour Dataset Trained Model trained with 5 iterations
+    # # ---------------------------------------------------------
     # net = new_piech_models.ContourIntegrationCSI(lateral_e_size=15, lateral_i_size=15, n_iters=5)
-    # saved_model = './results/new_model/ContourIntegrationCSI_20200130_181122_gaussian_reg_sigma_10_loss_e-5' \
-    #               '/best_accuracy.pth'
-    # replacement_layer = None
-    # net.load_state_dict(torch.load(saved_model))
-    # get_iou_results = True
-
-    # net = new_piech_models.get_embedded_resnet50_model()
-    # saved_model = './results/imagenet_classification/ResNet_20200208_153412' \
-    #               '/best_accuracy.pth'
-    # checkpoint = torch.load(saved_model)
-    # replacement_layer = net.conv1
-    # net.load_state_dict(checkpoint['state_dict'])
-    # get_iou_results = False
-
-    # net = new_piech_models.ContourIntegrationCSIResnet50(lateral_e_size=15, lateral_i_size=15, n_iters=5)
     # saved_model = \
-    #     './results/new_model_resnet_based/' \
-    #     'ContourIntegrationCSIResnet50_20200131_194615_gaussian_reg_sigma_10_weight_0.0001' \
+    #     './results/new_model/ContourIntegrationCSI_20200130_181122_gaussian_reg_sigma_10_loss_e-5' \
     #     '/best_accuracy.pth'
     # replacement_layer = None
     # net.load_state_dict(torch.load(saved_model))
     # get_iou_results = True
 
+    # # Contour Dataset Trained (Resnet50 edge extract) Model
+    # # ---------------------------------------------------------
+    # net = new_piech_models.ContourIntegrationCSIResnet50(lateral_e_size=15, lateral_i_size=15, n_iters=5)
+    # saved_model = \
+    #     './results/new_model_resnet_based/' \
+    #     'ContourIntegrationCSIResnet50_20200131_194615_gaussian_reg_sigma_10_weight_0.0001' \
+    #     '/best_accuracy.pth'
+    # saved_model = \
+    #     './results/imagenet_classification/' \
+    #     'ResNet_20200212_202439_untrained_gaussian_reg_epochs_12_momentum_0.9_lr_0.1' \
+    #     '/best_accuracy.pth'
+    # replacement_layer = None
+    # net.load_state_dict(torch.load(saved_model))
+    # get_iou_results = True
+
+    # # # Imagenet Trained Model
+    # # # ----------------------
+    # net = new_piech_models.get_embedded_resnet50_model()
+    # saved_model = \
+    #     './results/imagenet_classification/' \
+    #     'ResNet_20200212_202439_untrained_gaussian_reg_epochs_12_momentum_0.9_lr_0.1' \
+    #     '/best_accuracy.pth'
+    # checkpoint = torch.load(saved_model)
+    # replacement_layer = net.conv1
+    # net.load_state_dict(checkpoint['state_dict'])
+    # get_iou_results = False
+
+    # Edge Dataset Trained Model
+    # ----------------------------
     net = new_piech_models.EdgeDetectionCSIResnet50(lateral_e_size=15, lateral_i_size=15, n_iters=5)
-    saved_model = './results/edge_detection/EdgeDetectionCSIResnet50_20200220_160930_lr_e-4_doobnet_edge_extract' \
-                  '/best_accuracy.pth'
+    saved_model = \
+        './results/edge_detection/EdgeDetectionCSIResnet50_20200220_160930_lr_e-4_doobnet_edge_extract' \
+        '/best_accuracy.pth'
     net.load_state_dict(torch.load(saved_model))
     replacement_layer = None
     get_iou_results = False
@@ -67,16 +81,15 @@ if __name__ == "__main__":
     # Main Loop
     # -----------------------------------------------------------------------------------
     # frag_size_list = [(7, 7)]
-    frag_size_list = [(9, 9), (11, 11), (13, 13)]
-    # frag_size_list = [(13, 13)]
+    frag_size_list = [(7, 7), (9, 9), (11, 11), (13, 13)]
 
     for frag_size in frag_size_list:
         print("Processing Fragment Size {} {}".format(frag_size, '-'*50))
         frag_size = np.array(frag_size)
 
-        # experiment_gain_vs_len.main(
-        #     net, results_dir, iou_results=get_iou_results, embedded_layer_identifier=replacement_layer,
-        #     frag_size=frag_size)
+        experiment_gain_vs_len.main(
+            net, results_dir, iou_results=get_iou_results, embedded_layer_identifier=replacement_layer,
+            frag_size=frag_size)
 
         experiment_gain_vs_spacing.main(
             net, results_dir, embedded_layer_identifier=replacement_layer, frag_size=frag_size)
