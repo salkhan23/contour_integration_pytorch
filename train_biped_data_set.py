9c769571d07a264fe95821feeb568d5cdc70979d
Salman Khan <salman.khan@wimatek.com>
2020-04-23 21:43:22 -0400

    TEMP: Disable experiment scripts, to see if the who process if working fine first

--- train_biped_data_set.py
+++ train_biped_data_set.py
@@ -399,16 +399,16 @@ def main(model, train_params, data_set_params, base_results_store_dir='./results
     # -----------------------------------------------------------------------------------
     # Run Li 2006 experiments
     # -----------------------------------------------------------------------------------
-    print("====> Running Experiments")
-    experiment_gain_vs_len.main(model, base_results_dir=results_store_dir, iou_results=False)
-    experiment_gain_vs_len.main(
-        model,
-        base_results_dir=results_store_dir,
-        iou_results=False,
-        frag_size=np.array([11, 11])
-    )
-
-    experiment_gain_vs_spacing.main(model, base_results_dir=results_store_dir)
+    # print("====> Running Experiments")
+    # experiment_gain_vs_len.main(model, base_results_dir=results_store_dir, iou_results=False)
+    # experiment_gain_vs_len.main(
+    #     model,
+    #     base_results_dir=results_store_dir,
+    #     iou_results=False,
+    #     frag_size=np.array([11, 11])
+    # )
+    #
+    # experiment_gain_vs_spacing.main(model, base_results_dir=results_store_dir)
 
 
 if __name__ == '__main__':
