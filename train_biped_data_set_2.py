7fbf54a29bcbad57a7bb89fdb7852c0f6ebed879
Salman Khan <salman.khan@wimatek.com>
2020-04-29 23:45:42 -0400

    Minor restore train batch size

--- train_biped_data_set_2.py
+++ train_biped_data_set_2.py
@@ -461,7 +461,7 @@ if __name__ == '__main__':
     }
 
     train_parameters = {
-        'train_batch_size': 8,
+        'train_batch_size': 32,
         'test_batch_size': 1,
         'learning_rate': 1e-3,
         'num_epochs': 5,
