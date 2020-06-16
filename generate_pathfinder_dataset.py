a17d0df4414991bb31d626c145ab0b9b6e58cc55
Salman Khan <salman.khan@wimatek.com>
2020-06-16 19:34:08 -0400

    Refactor the code and create a train and a validation set

--- generate_pathfinder_dataset.py
+++ generate_pathfinder_dataset.py
@@ -9,54 +9,35 @@ import shutil
 import sys
 
 import torch
-import torchvision.transforms.functional as transform_functional
-import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 import dataset_pathfinder_natural_images
 
 
-if __name__ == "__main__":
-    # -----------------------------------------------------------------------------------
-    # Initialization
-    # -----------------------------------------------------------------------------------
-    data_store_dir = './data/pathfinder_natural_images_test'
-
-    # Total images generated will be biped_data_subset_size * n_epochs
-    biped_data_subset_size = 500
-    n_epochs = 10
-
-    random_seed = 8
-
-    # Immutable ----------------------
-    plt.ion()
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
+def create_dataset(data_dir, biped_dataset_type, n_biped_imgs, n_epochs):
 
     biped_dataset_dir = './data/BIPED/edges'
 
-    start_time = datetime.now()
-
     # Create the results store directory
     # ----------------------------------
-    if os.path.exists(data_store_dir):
-        ans = input("{} already  data. Overwrite ?".format(data_store_dir))
+    if os.path.exists(data_dir):
+        ans = input("{} already  data. Overwrite ?".format(data_dir))
         if 'y' in ans.lower():
-            shutil.rmtree(data_store_dir)
+            shutil.rmtree(data_dir)
         else:
             sys.exit()
 
-    imgs_dir = os.path.join(data_store_dir, 'images')
-    indv_contours_labels_dir = os.path.join(data_store_dir, 'individual_contours_labels')
-    full_labels_dir = os.path.join(data_store_dir, 'full_labels')
+    imgs_dir = os.path.join(data_dir, 'images')
+    indv_contours_labels_dir = os.path.join(data_dir, 'individual_contours_labels')
+    full_labels_dir = os.path.join(data_dir, 'full_labels')
     for folder in [imgs_dir, indv_contours_labels_dir, full_labels_dir]:
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-    data_key_filename = os.path.join(data_store_dir, 'data_key.txt')
-    class_labels_filename = os.path.join(data_store_dir, 'classification_labels.txt')
-    map_to_org_imgs_filename = os.path.join(data_store_dir, 'map_to_original_images.txt')
-    distances_filename = os.path.join(data_store_dir, 'distances_between_points.txt')
+    data_key_filename = os.path.join(data_dir, 'data_key.txt')
+    class_labels_filename = os.path.join(data_dir, 'classification_labels.txt')
+    map_to_org_imgs_filename = os.path.join(data_dir, 'map_to_original_images.txt')
+    distances_filename = os.path.join(data_dir, 'distances_between_points.txt')
 
     data_key_handle = open(data_key_filename, 'w+')
     class_labels_handle = open(class_labels_filename, 'w+')
@@ -64,31 +45,29 @@ if __name__ == "__main__":
     org_imgs_map_handle = open(map_to_org_imgs_filename, 'w+')
     distances_handle = open(distances_filename, 'w+')
 
-    # -----------------------------------------------------------------------------------
     # Setup the Online Pathfinder Data loader
-    # -----------------------------------------------------------------------------------
-    print("New Dataset will be created @ {}\n{}".format(data_store_dir, '*' * 80))
+    # ---------------------------------------
+    print("New Dataset will be created @ {}\n{}".format(data_dir, '*' * 80))
     start_time = datetime.now()
 
     data_set = dataset_pathfinder_natural_images.NaturalImagesPathfinder(
         data_dir=biped_dataset_dir,
-        dataset_type='train',
+        dataset_type=biped_dataset_type,
         transform=None,
-        subset_size=biped_data_subset_size,
+        subset_size=n_biped_imgs,
         resize_size=(256, 256),
     )
 
     data_loader = DataLoader(
         dataset=data_set,
         num_workers=0,
-        batch_size=1,   # must stay as one
+        batch_size=1,  # must stay as one
         shuffle=False,
         pin_memory=True
     )
 
-    # -----------------------------------------------------------------------------------
     # Main
-    # -----------------------------------------------------------------------------------
+    # -----
     dist_not_connected = []
     dist_connected = []
     n_imgs_created = 0
@@ -142,8 +121,8 @@ if __name__ == "__main__":
             n_imgs_created += 1
 
     print("Connected: m={:0.2f}, std={:0.2f}, Not connected: m={:0.2f}, std={:0.2f}".format(
-            np.mean(dist_connected), np.std(dist_connected),
-            np.mean(dist_not_connected), np.std(dist_not_connected)))
+        np.mean(dist_connected), np.std(dist_connected),
+        np.mean(dist_not_connected), np.std(dist_not_connected)))
 
     f, ax_arr = plt.subplots(2, 1, sharex=True, figsize=(9, 9))
     connected_hist = ax_arr[0].hist(dist_connected, bins=np.arange(0, 300, 50))
@@ -161,18 +140,56 @@ if __name__ == "__main__":
     ax_arr[0].set_ylim(0, y_max * 1.1)
     ax_arr[1].set_ylim(0, y_max * 1.1)
 
-    f.savefig(os.path.join(data_store_dir, 'histogram.jpg'), format='jpg')
+    f.savefig(os.path.join(data_dir, 'histogram.jpg'), format='jpg')
 
-    # -----------------------------------------------------------------------------------
-    # End
-    # -----------------------------------------------------------------------------------
     data_key_handle.close()
     class_labels_handle.close()
     org_imgs_map_handle.close()
     distances_handle.close()
 
     print("Dataset Created. Number images {} from {} unique images. Time {}".format(
-        n_imgs_created, biped_data_subset_size, datetime.now() - start_time))
+        n_imgs_created, n_biped_imgs, datetime.now() - start_time))
 
+
+if __name__ == "__main__":
+    # -----------------------------------------------------------------------------------
+    # Initialization
+    # -----------------------------------------------------------------------------------
+    data_store_dir = './data/pathfinder_natural_images_2'
+
+    random_seed = 8
+
+    # Immutable ----------------------
+    plt.ion()
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+
+    # ----------------------------------------------------------------------------------
+    # Main
+    # ----------------------------------------------------------------------------------
+    num_biped_imgs = 10000   # Total images generated will be num_biped_imgs * num_epochs
+    num_epochs = 1
+
+    print("Creating The Training Dataset ...")
+    create_dataset(
+        os.path.join(data_store_dir, 'train'),
+        'train',
+        num_biped_imgs,
+        num_epochs
+    )
+
+    print("Creating The Testing Dataset ...")
+    num_biped_imgs = 50
+    num_epochs = 10
+    create_dataset(
+        os.path.join(data_store_dir, 'test'),
+        'test',
+        num_biped_imgs,
+        num_epochs
+    )
+
+    # -----------------------------------------------------------------------------------
+    # End
+    # -----------------------------------------------------------------------------------
     import pdb
     pdb.set_trace()
