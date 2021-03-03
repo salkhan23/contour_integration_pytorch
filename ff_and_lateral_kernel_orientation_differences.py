55854b3705517df6b46a7a99717f457c680e50c7
Salman Khan <salman.sw@riotmicro.com>
2021-03-03 08:40:08 -0500

    1. Allow the channel aggregation function to be input specifiable. Some improvements to the plotted figures

--- ff_and_lateral_kernel_orientation_differences.py
+++ ff_and_lateral_kernel_orientation_differences.py
@@ -3,8 +3,8 @@
 # Do gabor fits on them
 # And look at Gabor fit orientation Differences
 # ---------------------------------------------------------------------------------------
-
 import pickle
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -14,6 +14,12 @@ import torch
 import gabor_fits
 import models.new_piech_models as new_piech_models
 import utils
+
+mpl.rcParams.update({
+    'font.size': 18, 'lines.linewidth': 3,
+    'lines.markersize': 10,
+    'lines.markeredgewidth': 3
+})
 
 
 def find_ff_kernel_orientations(weights):
@@ -29,9 +35,10 @@ def find_ff_kernel_orientations(weights):
     return ori_arr
 
 
-def find_lateral_kernels_orientations(weights):
+def find_lateral_kernels_orientations(weights, ch_aggregator_fcn):
     """
 
+    :param ch_aggregator_fcn: function to squash channel dimension
     :param weights:  [out_ch, in_ch, r, c]
     :return:
     """
@@ -41,7 +48,7 @@ def find_lateral_kernels_orientations(weights):
     for n_idx in range(n):
         print("Processing kernel {}".format(n_idx))
         kernel = weights[n_idx, ]
-        kernel_flattened = np.sum(kernel, axis=0)  # flattened across input channels
+        kernel_flattened = ch_aggregator_fcn(kernel, axis=0)  # flattened across input channels
         kernel_flattened = np.expand_dims(kernel_flattened, axis=-1)  # channel last, expanded
 
         ori_arr[n_idx] = gabor_fits.get_filter_orientation(kernel_flattened, o_type='max', display_params=False)
@@ -61,7 +68,7 @@ def plot_ff_lat_orientation_differences(ff_ori, lateral_e_ori, lateral_i_ori):
     f, ax_arr = plt.subplots(figsize=(9, 9))
 
     ax_arr.scatter(ff_ori, lateral_e_ori, label='E')
-    ax_arr.scatter(ff_ori, lateral_i_ori, label='I')
+    ax_arr.scatter(ff_ori, lateral_i_ori, label='I', marker='x')
     ax_arr.set_xlim([-90, 90])
     ax_arr.set_ylim([-90, 90])
     ax_arr.legend()
@@ -74,12 +81,16 @@ def plot_ff_lat_orientation_differences(ff_ori, lateral_e_ori, lateral_i_ori):
 
     plt.title("Gabor Fits Orientations")
 
+    import pdb
+    pdb.set_trace()
+
     return f, ax_arr
 
 
-def find_ff_and_lateral_orientation_preferences(model, results_store_dir=None):
+def find_ff_and_lateral_orientation_preferences(model, ch_aggregator_fcn=np.mean, results_store_dir=None):
     """
 
+    :param ch_aggregator_fcn:
     :param model:
     :param results_store_dir:
     :return:
@@ -93,7 +104,7 @@ def find_ff_and_lateral_orientation_preferences(model, results_store_dir=None):
     utils.view_spatial_lateral_kernels(
         model.contour_integration_layer.lateral_e.weight.data.numpy(),
         model.contour_integration_layer.lateral_i.weight.data.numpy(),
-        spatial_func=np.mean,
+        spatial_func=ch_aggregator_fcn,
         results_store_dir=results_store_dir,
     )
 
@@ -126,8 +137,10 @@ def find_ff_and_lateral_orientation_preferences(model, results_store_dir=None):
     else:
         print("Finding Lateral Kernel Orientations")
         lateral_kernels_orientations = {
-            'E': find_lateral_kernels_orientations(model.contour_integration_layer.lateral_e.weight.data.numpy()),
-            'I': find_lateral_kernels_orientations(model.contour_integration_layer.lateral_i.weight.data.numpy())
+            'E': find_lateral_kernels_orientations(
+                model.contour_integration_layer.lateral_e.weight.data.numpy(), ch_aggregator_fcn),
+            'I': find_lateral_kernels_orientations(
+                model.contour_integration_layer.lateral_i.weight.data.numpy(), ch_aggregator_fcn)
         }
 
         if results_store_dir is not None:
@@ -158,6 +171,7 @@ def find_ff_and_lateral_orientation_preferences(model, results_store_dir=None):
 if __name__ == '__main__':
 
     store_results = True
+    channel_aggregator_fcn = np.sum
 
     # -----------------------------------------------------------------------------------
     # Model
@@ -167,7 +181,7 @@ if __name__ == '__main__':
     net = new_piech_models.ContourIntegrationResnet50(cont_int_layer)
     saved_model = \
         './results/multiple_runs_contour_dataset/positive_lateral_weights_with_BN_best_gain_curves/' \
-        '/run_5' \
+        '/run_2' \
         '/best_accuracy.pth'
 
     # -------------------------------------
@@ -180,12 +194,17 @@ if __name__ == '__main__':
 
     if store_results:
         store_results_dir = os.path.dirname(saved_model)
-        store_results_dir = os.path.join(store_results_dir, 'lateral_kernels/preferred_orientations')
+        store_results_dir = os.path.join(
+            store_results_dir,
+            'lateral_kernels/preferred_orientations/{}'.format(channel_aggregator_fcn.__name__))
 
         if not os.path.exists(store_results_dir):
             os.makedirs(store_results_dir)
 
-    find_ff_and_lateral_orientation_preferences(net, results_store_dir=store_results_dir)
+    find_ff_and_lateral_orientation_preferences(
+        net,
+        ch_aggregator_fcn=channel_aggregator_fcn,
+        results_store_dir=store_results_dir)
 
     # -----------------------------------------------------------------------------------
     # End
