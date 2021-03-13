2e1fee896f0e891914d360ae96fec021ac6fadb4
Salman Khan <salman.sw@riotmicro.com>
2021-03-13 14:04:29 -0500

    Update script.

--- analyze_lateral_rf_size.py
+++ analyze_lateral_rf_size.py
@@ -1,21 +1,14 @@
 # -------------------------------------------------------------------------------------
 #  Call contour data set training script with different Lateral weight sizes.
 # -------------------------------------------------------------------------------------
-import numpy as np
-import torch
-
 from train_contour_data_set import main
 import models.new_piech_models as new_piech_models
 
 
 if __name__ == '__main__':
-    random_seed = 10
-    torch.manual_seed(random_seed)
-    np.random.seed(random_seed)
 
     lateral_rf_size_arr = [3, 5, 7, 11, 15, 19, 23, 27, 31, 35]
 
-    # ----------------------------------------------------------------------
     data_set_parameters = {
         'data_set_dir': "./data/channel_wise_optimal_full14_frag7",
         'train_subset_size': 20000,
@@ -23,12 +16,16 @@ if __name__ == '__main__':
     }
 
     train_parameters = {
+        'random_seed': 1,
         'train_batch_size': 32,
-        'test_batch_size': 1,
-        'learning_rate': 3e-5,
-        'num_epochs': 50,
+        'test_batch_size': 32,
+        'learning_rate': 1e-4,
+        'num_epochs': 100,
         'lateral_w_reg_weight': 0.0001,
         'lateral_w_reg_gaussian_sigma': 10,
+        'clip_negative_lateral_weights': True,
+        'lr_sched_step_size': 80,
+        'lr_sched_gamma': 0.5
     }
 
     for lateral_rf_size in lateral_rf_size_arr:
@@ -37,7 +34,12 @@ if __name__ == '__main__':
         base_results_dir = './results/lateral_rf_explore/size_{}'.format(lateral_rf_size)
 
         cont_int_layer = new_piech_models.CurrentSubtractInhibitLayer(
-            lateral_e_size=lateral_rf_size, lateral_i_size=lateral_rf_size, n_iters=5)
+            lateral_e_size=lateral_rf_size,
+            lateral_i_size=lateral_rf_size,
+            n_iters=5,
+            use_recurrent_batch_norm=True
+        )
+
         model = new_piech_models.ContourIntegrationAlexnet(cont_int_layer)
 
         main(
