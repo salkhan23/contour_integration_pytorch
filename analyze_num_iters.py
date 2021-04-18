8de99c5b6e75422544e64f2fb53078005dfc9be6
Salman Khan <salman.sw@riotmicro.com>
2021-04-17 23:19:12 -0400

    update script to the new  training script settings

--- analyze_num_iters.py
+++ analyze_num_iters.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     random_seed = 10
     torch.manual_seed(random_seed)
 
-    n_iters_arr = [1, 2, 3, 5, 8, 10, 15, 20, 25, 30]
+    n_iters_arr = [1, 2, 3, 5, 8, 10, 15, 20, 25]
     # n_iters_arr = n_iters_arr[::-1]
 
     # ----------------------------------------------------------------------
@@ -23,12 +23,16 @@ if __name__ == '__main__':
     }
 
     train_parameters = {
+        'random_seed': random_seed,
         'train_batch_size': 32,
-        'test_batch_size': 1,
-        'learning_rate': 3e-5,
-        'num_epochs': 50,
+        'test_batch_size': 32,
+        'learning_rate': 1e-4,
+        'num_epochs': 100,
         'lateral_w_reg_weight': 0.0001,
         'lateral_w_reg_gaussian_sigma': 10,
+        'clip_negative_lateral_weights': True,
+        'lr_sched_step_size': 80,
+        'lr_sched_gamma': 0.5
     }
 
     for n_iters in n_iters_arr:
@@ -37,7 +41,7 @@ if __name__ == '__main__':
         base_results_dir = './results/num_iteration_explore_2/n_iters_{}'.format(n_iters)
 
         cont_int_layer = new_piech_models.CurrentSubtractInhibitLayer(
-            lateral_e_size=15, lateral_i_size=15, n_iters=n_iters)
+            lateral_e_size=15, lateral_i_size=15, n_iters=n_iters, use_recurrent_batch_norm=True)
         model = new_piech_models.ContourIntegrationAlexnet(cont_int_layer)
 
         main(
