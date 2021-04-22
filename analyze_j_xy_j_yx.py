4be74ff948cde47d8cd86a5c98db512d43ae03a0
Salman Khan <salman.sw@riotmicro.com>
2021-04-22 00:01:07 -0400

    Update script to use new params of the model

--- analyze_j_xy_j_yx.py
+++ analyze_j_xy_j_yx.py
@@ -10,11 +10,11 @@ import models.new_piech_models as new_piech_models
 from train_utils import inverse_sigmoid
 
 if __name__ == '__main__':
-    random_seed = 1
+    random_seed = 7
     torch.manual_seed(random_seed)
     np.random.seed(random_seed)
 
-    sigma_jxy_jyx_range = np.array([0.001, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.999])
+    sigma_jxy_jyx_range = np.array([0.001, 0.2, 0.4, 0.6, 0.8, 0.999])
     jxy_jyx_range = inverse_sigmoid(sigma_jxy_jyx_range)
 
     # ----------------------------------------------------------------------
@@ -25,26 +25,28 @@ if __name__ == '__main__':
     }
 
     train_parameters = {
+        'random_seed': random_seed,
         'train_batch_size': 32,
-        'test_batch_size': 1,
+        'test_batch_size': 32,
         'learning_rate': 1e-4,
-        'lr_sched_step_size': 40,
-        'num_epochs': 60,
-        'gaussian_reg_weight': 0.0001,
-        'gaussian_reg_sigma': 10,
-        'clip_negative_lateral_weights': False
+        'num_epochs': 100,
+        'lateral_w_reg_weight': 0.0001,
+        'lateral_w_reg_gaussian_sigma': 10,
+        'clip_negative_lateral_weights': True,
+        'lr_sched_step_size': 80,
+        'lr_sched_gamma': 0.5
     }
 
     for jxy_jyx in jxy_jyx_range:
         print("Processing jxy_jyx = {}".format(jxy_jyx, '*' * 40))
 
         cont_int_layer = new_piech_models.CurrentSubtractInhibitLayer(
-            lateral_e_size=15, lateral_i_size=15, n_iters=5, j_xy=jxy_jyx, j_yx=jxy_jyx)
+            lateral_e_size=15, lateral_i_size=15, n_iters=5, j_xy=jxy_jyx, use_recurrent_batch_norm=True)
 
         net = new_piech_models.ContourIntegrationResnet50(cont_int_layer)
 
         main(net, train_params=train_parameters, data_set_params=data_set_parameters,
-             base_results_store_dir='./results/explore_fixed_jxy_jyx/jxy_jyx_{:0.3}'.format(jxy_jyx))
+             base_results_store_dir='./results/explore_fixed_jxy/jxy_{:0.3}'.format(jxy_jyx))
 
     # -----------------------------------------------------------------------------------
     # End
