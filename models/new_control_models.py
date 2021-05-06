0e2195b69588f52a8d7d60d89199c05b9e9b8987
Salman Khan <salman.sw@riotmicro.com>
2021-05-06 16:48:31 -0400

    Comment out the sue of recurrent BN and use the same method as used by the original Model

--- models/new_control_models.py
+++ models/new_control_models.py
@@ -68,9 +68,11 @@ class ControlMatchIterationsLayer(nn.Module):
             bias=False
         )
 
-        self.control_bn1 = RecurrentBatchNorm(num_features=edge_out_ch, n_iters=5)
-        # self.control_bn1 = nn.BatchNorm2d(num_features=edge_out_ch)
-        # self.control_dp1 = nn.Dropout(p=0.3)
+        # self.control_bn1 = RecurrentBatchNorm(num_features=edge_out_ch, n_iters=5)
+        self.control_bn1 = nn.ModuleList([])
+        for i in range(self.n_iters):
+            self.control_bn1.append(nn.BatchNorm2d(num_features=edge_out_ch))
+        self.control_dp1 = nn.Dropout(p=0.3)
 
         self.lateral_i = nn.Conv2d(
             in_channels=edge_out_ch,
@@ -81,20 +83,22 @@ class ControlMatchIterationsLayer(nn.Module):
             bias=False
         )
 
-        self.control_bn2 = RecurrentBatchNorm(num_features=edge_out_ch, n_iters=5)
-        # self.control_bn2 = nn.BatchNorm2d(num_features=edge_out_ch)
-        # self.control_dp2 = nn.Dropout(p=0.3)
+        # self.control_bn2 = RecurrentBatchNorm(num_features=edge_out_ch, n_iters=5)
+        self.control_bn2 = nn.ModuleList([])
+        for i in range(self.n_iters):
+            self.control_bn2.append(nn.BatchNorm2d(num_features=edge_out_ch))
+        self.control_dp2 = nn.Dropout(p=0.3)
 
     def forward(self, ff):
 
         for i in range(self.n_iters):
             ff = self.lateral_e(ff)
-            ff = self.control_bn1(ff, i)
+            ff = self.control_bn1[i](ff)
             ff = nn.functional.relu(ff)
             # ff = self.control_dp1(ff)
 
             ff = self.lateral_i(ff)
-            ff = self.control_bn2(ff, i)
+            ff = self.control_bn2[i](ff)
             ff = nn.functional.relu(ff)
             # ff = self.control_dp2(ff)
 
