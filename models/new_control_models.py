aba76031f3607039e923b80bf2ed2216698d8cdf
Salman Khan <salman.sw@riotmicro.com>
2021-05-08 00:34:45 -0400

    Add a new control model based on  Recurrent convolutional neural networks

--- models/new_control_models.py
+++ models/new_control_models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torchvision
 from .new_piech_models import RecurrentBatchNorm
@@ -105,3 +106,66 @@ class ControlMatchIterationsLayer(nn.Module):
             # ff = self.control_dp2(ff)
 
         return ff
+
+
+class ControlRecurrentCNN(nn.Module):
+    def __init__(self, edge_out_ch=64, n_iters=5, lateral_e_size=7, lateral_i_size=7):
+        """
+        Model with matching number of recurrent iterations and capacity as the Contour Integration Layer.
+        Component interactions modeled after Recurrent Convolutional Neural Networks Liang and Hu - 2015 -
+        Recurrent Convolutional Neural Networks for Object Recognition
+
+        h(t) = sigma( W_x * X + W_h * h(t-1) + b)
+
+        Original model used Bias term, here the bias is removed and a batch normalization layer is used in its place.
+
+        :param edge_out_ch:
+        :param n_iters:
+        :param lateral_e_size:
+        :param lateral_i_size:
+        """
+        super(ControlRecurrentCNN, self).__init__()
+
+        self.lateral_e_size = lateral_e_size
+        self.lateral_i_size = lateral_i_size
+        self.edge_out_ch = edge_out_ch
+        self.n_iters = n_iters
+
+        self.lateral_e = nn.Conv2d(
+            in_channels=edge_out_ch,
+            out_channels=edge_out_ch,
+            kernel_size=self.lateral_e_size,
+            stride=1,
+            padding=(self.lateral_e_size - 1) // 2,
+            bias=False
+        )
+
+        self.lateral_i = nn.Conv2d(
+            in_channels=edge_out_ch,
+            out_channels=edge_out_ch,
+            kernel_size=self.lateral_i_size,
+            stride=1,
+            padding=(self.lateral_i_size - 1) // 2,
+            bias=False
+        )
+
+        # self.control_bn1 = RecurrentBatchNorm(num_features=edge_out_ch, n_iters=5)
+        self.control_bn = nn.ModuleList([])
+        for i in range(self.n_iters):
+            self.control_bn.append(nn.BatchNorm2d(num_features=edge_out_ch))
+
+        # self.dp = nn.Dropout(p=0.3)
+
+    def forward(self, ff):
+
+        h = torch.zeros_like(ff)  # hidden state
+
+        for i in range(self.n_iters):
+
+            h = self.lateral_e(ff) + self.lateral_i(h)
+            h = self.bn[i](h)
+            h = nn.functional.relu(h)
+
+            # h = self.dp(h)
+
+        return h
